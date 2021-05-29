468cb7c1c1e810e2048a8a087fffcb1a842143db
Eduardo Cavazos <wayo.cavazos@gmail.com>
2021-05-29 11:51:42 -0700

    Program.cs - DbInitializer.Initialize

--- Program.cs
+++ Program.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NutritionTrackerRazorPages.Data;
 
 namespace NutritionTrackerRazorPages
 {
@@ -13,7 +15,23 @@ namespace NutritionTrackerRazorPages
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                var context = services.GetRequiredService<NutritionTrackerContext>();
+
+                context.Database.EnsureCreated();
+
+                DbInitializer.Initialize(context);
+            }
+                
+                
+            host.Run();
+
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
